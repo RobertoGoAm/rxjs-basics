847b03f897f8a23d368e72450c5d6e13c160faea
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-19 14:59:45 +0200

    Getting started with Operators - Transform streams using map, pluck, and mapTo

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 import 'regenerator-runtime/runtime';
-import { interval, timer } from "rxjs";
+import {of, fromEvent } from "rxjs";
+import { map, mapTo, pluck } from "rxjs/operators";
 
-const timer$ = timer(2000);
+// of(1,2,3,4,5).pipe(
+    // map(value => value * 10)
+// ).subscribe(console.log);
 
-timer$.subscribe(console.log);
+const keyup$ = fromEvent(document, 'keyup');
+const keycode$ = keyup$.pipe(
+    map(event => event.code)
+);
+const keycodeWithPluck$ = keyup$.pipe(
+    pluck('code')
+);
+const pressed$ = keyup$.pipe(
+    mapTo('Key Pressed!')
+);
+
+pressed$.subscribe(console.log);
