c80343de3db452ad9c5af7789afa68d97ae03cc0
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-21 12:15:52 +0200

    Getting Started with Operators - Manage state changes incrementally with scan

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 import 'regenerator-runtime/runtime';
-import { from, interval } from "rxjs";
-import { reduce, take } from "rxjs/operators";
+import { from } from "rxjs";
+import { map, reduce, scan } from "rxjs/operators";
 
 const numbers = [1,2,3,4,5];
+const users = [
+    { name: 'Brian', loggedIn: false, token: null },
+    { name: 'Brian', loggedIn: true, token: 'abc' },
+    { name: 'Brian', loggedIn: true, token: '123' }
+];
 
-const totalReducer = (accumulator, currentValue) => {
-    return accumulator + currentValue;
-};
+const state$ = from(users).pipe(
+    scan((accumulator, currentValue) => {
+        return { ...accumulator, ...currentValue };
+    }, {})
+)
 
-interval(1000).pipe(
-    take(3),
-    reduce(totalReducer, 0)
-).subscribe({
-    next: console.log,
-    complete: () => console.log('Complete!')
-});
+const name$ = state$.pipe(
+    map(state => state.name)
+);
+
+name$.subscribe(console.log)
