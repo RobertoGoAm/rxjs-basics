4755cd4224919838039bb7d2a4c2db4101fac51d
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-24 12:59:45 +0200

    Rate Limiting Operators - Sample a stream on a uniform duration using sampleTime

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 import 'regenerator-runtime/runtime';
-import { asyncScheduler, fromEvent } from 'rxjs';
-import { map, throttleTime, tap } from 'rxjs/operators';
+import { fromEvent, interval } from 'rxjs';
+import { sample, map } from "rxjs/operators";
 
-function calculateScrollPercent(element) {
-  const { scrollTop, scrollHeight, clientHeight } = element;
+const click$ = fromEvent(document, 'click');
+const timer$ = interval(1000);
 
-  return (scrollTop / (scrollHeight - clientHeight)) * 100;
-}
-
-// Elements
-const progressBar = document.querySelector(
-  '.progress-bar'
-);
-
-// Streams
-const scroll$ = fromEvent(document, 'scroll');
-const progress$ = scroll$.pipe(
-  throttleTime(30, asyncScheduler, {
-    leading: false,
-    trailing: true
-  }),
-  map(({ target }) => calculateScrollPercent(
-    target.scrollingElement
-  )),
-  tap(console.log)
-);
- 
-progress$.subscribe(percent => {
-  progressBar.style.width = `${percent}%`;
-});
+timer$.pipe(
+  sample(click$)
+).subscribe(console.log);
