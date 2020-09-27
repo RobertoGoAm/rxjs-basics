87f1207b2566d2d992a54cdbda28474b181df26b
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-27 16:33:32 +0200

    Transformation Operators - Catch errors on observables with catchError

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
 import 'regenerator-runtime/runtime';
 import { ajax } from "rxjs/ajax";
-import { fromEvent } from 'rxjs';
-import { exhaustMap } from "rxjs/operators";
+import { fromEvent, EMPTY, interval } from 'rxjs';
+import {
+  debounceTime,
+  pluck,
+  distinctUntilChanged,
+  switchMap,
+  catchError
+} from "rxjs/operators";
 
-const authenticateUser = () => {
-  return ajax.post(
-    'https://reqres.in/api/login',
-    {
-      email: 'eve.holt@reqres.in',
-      password: 'cityslicka'
-    }
-  )
-};
+const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
 // Elements
-const loginButton = document.getElementById(
-  'login'
+const inputBox = document.getElementById(
+  'text-input'
+);
+const typeaheadContainer = document.getElementById(
+  'typeahead-container'
 );
 
 // Streams
-const login$ = fromEvent(loginButton, 'click');
+const input$ = fromEvent(inputBox, 'keyup');
 
-login$.pipe(
-  exhaustMap(() => authenticateUser())
-).subscribe(console.log);
+input$.pipe(
+  debounceTime(200),
+  pluck('target', 'value'),
+  distinctUntilChanged(),
+  switchMap(searchTerm => {
+    return ajax.getJSON(
+      `${BASE_URL}?by_name=${searchTerm}`
+    ).pipe(
+      catchError((error, caught) => {
+        return caught;
+      })
+    )
+  })
+).subscribe(response => {
+  typeaheadContainer.innerHTML = response.map(b => b.name).join('<br>');
+})
