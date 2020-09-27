accb15c280d6f9c7c6b4481733b461470b19fdf4
Roberto Gomez <RobertoGo@protonmail.com>
2020-09-27 17:27:30 +0200

    Transformation Operators - Lab 3. Create an HTTP polling solution

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
 import 'regenerator-runtime/runtime';
 import { ajax } from "rxjs/ajax";
-import { fromEvent, EMPTY, interval } from 'rxjs';
-import {
-  debounceTime,
-  pluck,
-  distinctUntilChanged,
-  switchMap,
-  catchError
-} from "rxjs/operators";
+import { fromEvent, timer } from 'rxjs';
+import { takeUntil, pluck, mergeMapTo, exhaustMap, tap, finalize, switchMapTo } from "rxjs/operators";
 
-const BASE_URL = 'https://api.openbrewerydb.org/breweries';
 
 // Elements
-const inputBox = document.getElementById(
-  'text-input'
+const startButton = document.getElementById(
+  'start'
 );
-const typeaheadContainer = document.getElementById(
-  'typeahead-container'
+const stopButton = document.getElementById(
+  'stop'
+);
+const pollingStatus = document.getElementById(
+  'polling-status'
+);
+const dogImage = document.getElementById(
+  'dog'
 );
 
 // Streams
-const input$ = fromEvent(inputBox, 'keyup');
+const startClick$ =  fromEvent(startButton, 'click');
+const stopClick$ =  fromEvent(stopButton, 'click');
 
-input$.pipe(
-  debounceTime(200),
-  pluck('target', 'value'),
-  distinctUntilChanged(),
-  switchMap(searchTerm => {
-    return ajax.getJSON(
-      `${BASE_URL}?by_name=${searchTerm}`
-    ).pipe(
-      catchError((error, caught) => {
-        return caught;
-      })
-    )
-  })
-).subscribe(response => {
-  typeaheadContainer.innerHTML = response.map(b => b.name).join('<br>');
-})
+startClick$.pipe(
+  exhaustMap(timer(0, 5000).pipe(
+    tap(() => pollingStatus.innerHTML = 'Active'),
+    switchMapTo(
+      ajax.getJSON(
+        'https://random.dog/woof.json'
+      ).pipe(
+        pluck('url')
+      )
+    ),
+    takeUntil(stopClick$),
+    finalize(() => pollingStatus.innerHTML = 'Stopped')
+  ))
+).subscribe(url => dogImage.src = url);
