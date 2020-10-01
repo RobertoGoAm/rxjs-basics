9a564a6ab9fd934a5638e5b694079ef2e8112600
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-01 13:05:05 +0200

    Combination Operators - Lab 4. Create a Mortgage Calculator

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-import 'regenerator-runtime/runtime';
-import { of, forkJoin } from "rxjs";
+import "regenerator-runtime/runtime";
+import { combineLatest, fromEvent, of } from "rxjs";
+import { delay, filter, map, mergeMap, share } from "rxjs/operators";
 
-const GITHUB_API_BASE = 'http://api.github.com';
+function calculateMortgage(interest, loanAmount, loanLength) {
+  const calculatedInterest = interest / 1200;
+  const total =
+    (loanAmount * calculatedInterest) /
+    (1 - Math.pow(1 / (1 + calculatedInterest), loanLength));
 
-forkJoin(
-  {
-    user: ajax.getJSON(
-      `${GITHUB_API_BASE}/users/reactivex`
-    ),
-    repo: ajax.getJSON(
-      `${GITHUB_API_BASE}/users/reactivex/repos`
-    ),
-  }
-).subscribe(console.log);
+  return total.toFixed(2);
+}
+
+const loanAmount = document.getElementById("loanAmount");
+const interest = document.getElementById("interest");
+const loanLength = document.querySelectorAll(".loanLength");
+const expected = document.getElementById("expected");
+
+// Helpers
+const createInputValueStream = elem => {
+  return fromEvent(elem, 'input').pipe(
+    map(event => parseFloat(event.target.value))
+  )
+};
+
+const saveResponse = mortageAmount => {
+  return of(mortageAmount).pipe(
+    delay(1000)
+  )
+}
+
+// Stream
+const interest$ = createInputValueStream(interest);
+const loanLength$ = createInputValueStream(loanLength);
+const loanAmount$ = createInputValueStream(loanAmount);
+
+const calculation$ = combineLatest([
+  interest$,
+  loanLength$,
+  loanAmount$,
+]).pipe(
+  map(([interest, loanAmount, loanLength]) => {
+    return calculateMortgage(
+      interest, loanAmount, loanLength
+    )
+  }),
+  filter(mortageAmount => !isNaN(mortageAmount)),
+  share()
+);
+
+calculation$.subscribe(mortageAmount => {
+  expected.innerHTML = mortageAmount;
+});
+
+calculation$.pipe(
+  mergeMap(mortageAmount => saveResponse(
+    mortageAmount
+  ))
+).subscribe();
