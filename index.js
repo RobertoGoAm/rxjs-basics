4c94909eefbd8fb68df0f065b1e39a4a66fae486
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-06 12:32:57 +0200

    Intro to Schedulers - Execute tasks asynchronously with Async Scheduler

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 import "regenerator-runtime/runtime";
-import { AsyncSubject } from "rxjs";
+import { asyncScheduler, of } from "rxjs";
 
 const observer = {
   next: val => console.log('next', val),
   error: err => console.log('error', err),
   complete: () => console.log('complete')
 };
-
-const subject = new AsyncSubject();
-
-subject.subscribe(observer);
-subject.subscribe(observer);
-
-subject.next('Hello');
-subject.next('World');
-subject.next('Goodbye');
-
-subject.complete();
+// const sub = asyncScheduler.schedule(
+  // console.log,
+  // 2000,
+  // "Hello World!"
+// );
+// sub.unsubscribe();
+of(4,5,6).pipe(
+  tap(val => console.log('from tap', val)),
+  subscribeOn(asyncScheduler, 3000)
+).subscribe(observer);
