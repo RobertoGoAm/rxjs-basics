a1fbfc5a45b6047e452445a7989eb04af6a231d6
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-07 11:56:29 +0200

    Intro to Schedulers - Execute tasks on a queue with Queue Scheduler

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 import "regenerator-runtime/runtime";
-import { animationFrameScheduler, asapScheduler, asyncScheduler, interval, range } from "rxjs";
-import { takeWhile } from "rxjs/operators";
+import { queueScheduler } from "rxjs";
 
-const observer = {
-  next: val => console.log('next', val),
-  error: err => console.log('error', err),
-  complete: () => console.log('complete')
-};
-
-const ball = document.getElementById('ball');
-
-// animationFrameScheduler.schedule(function(position) {
-//   ball.style.transform = `translate3d(0, ${position}px, 0)`;
-
-//   if (position <= 300) {
-//     this.schedule(position + 1);
-//   }
-// }, 0, 0);
-
-interval(0, animationFrameScheduler).pipe(
-  takeWhile(val => val <= 300)
-).subscribe(val => {
-  ball.style.transform = `translate3d(0, ${val}px, 0)`;
-})
+queueScheduler.schedule(() => {
+  queueScheduler.schedule(() => {
+    queueScheduler.schedule(() => {
+      console.log('second inner queue');
+    });
+    console.log('inner queue');
+  });
+  console.log('first queue');
+});
+console.log('sync');
