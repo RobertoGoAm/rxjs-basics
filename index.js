35d2d241f1b28c0265895773b9c99a72396fa238
Roberto Gomez <RobertoGo@protonmail.com>
2020-10-07 11:18:27 +0200

    Intro to Schedulers - Defer task execution with the Asap Scheduler

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 import "regenerator-runtime/runtime";
-import { asyncScheduler, of } from "rxjs";
+import { asapScheduler, asyncScheduler, range } from "rxjs";
 
 const observer = {
   next: val => console.log('next', val),
   error: err => console.log('error', err),
   complete: () => console.log('complete')
 };
-// const sub = asyncScheduler.schedule(
-  // console.log,
-  // 2000,
-  // "Hello World!"
-// );
-// sub.unsubscribe();
-of(4,5,6).pipe(
-  tap(val => console.log('from tap', val)),
-  subscribeOn(asyncScheduler, 3000)
-).subscribe(observer);
+
+const counter = document.getElementById('counter');
+
+range(1,100000, asyncScheduler).subscribe(val => {
+  counter.innerHTML = val
+});
+
+console.log('synchronous console.log');
